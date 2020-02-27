e1359357c615b28a56c44de0bd14dcc5f32f2dc1
Paulo Italo <paulo.italo@esp.ce.gov.br>
2020-02-27 15:01:47 -0300

    finish question screen

--- src/views/Question.vue
+++ src/views/Question.vue
@@ -1,30 +1,95 @@
 <template>
-    <div class="background">
+    <div>
         <b-container>
             <h5 class="text-center text-uppercase">Questão {{currentIndex + 1}}/{{questionCount}}</h5>
             <b-row>
                 <b-col cols=12>
-                    <div class="timer"></div>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" :style="{'width': percent}" aria-valuenow="0" aria-valuemin="0" aria-valuemax="60"></div>
+                        <p class="position-absolute timer" style="left: 50%;">{{ timeRest }}</p>
+                    </div>
                     <div>
-                        <span v-for="(question, index) in questions" :key="index">
-                        </span>
+                        <div v-for="(question, index) in questions" :key="index" class="m-2 number-question" :class="{'number-question-active': currentIndex == index }">
+                            <p class="text-center " >
+                                {{index + 1}}
+                            </p>
+                        </div>
                     </div>
                 </b-col>
             </b-row>
             <b-row class="mx-3">
                 <p>{{ questions[currentIndex].title }}</p>
             </b-row>
-            <b-row>
+            <b-row class="mx-3">
                 <b-col cols="12" md=6 class="alternatives" v-for="(alternative, index) in questions[currentIndex].alternatives" :key="index">
-                    {{ alternative }}
+                    <button class="alternative my-2">
+                        <b-row class="py-2">
+                            <b-col cols=2 class="character">
+                                {{ alternative.character }}
+                            </b-col>
+                            <b-col cols=10>
+                                {{ alternative.answer }}
+                            </b-col>
+                        </b-row>
+                    </button>
                 </b-col>
             </b-row>
-            <Button class="text-center" :id="'formSubmit'" :color="'success'" :label="'Confirmar'" :icon="'icon-arrow'" />
+            <Button class="text-center py-3" :id="'formSubmit'" :color="'success'" :label="'Confirmar'" :icon="'icon-arrow'" />
         </b-container>
     </div>
 </template>
 
+<style lang="scss" scoped>
+    .progress {
+        height: 24px;
+        border: 2px solid  $burning-orange;
+        border-radius: 12px;
+        background-color: white;
+    }
+    .progress-bar {
+        background: linear-gradient(90deg, #FDA720 0%, $burning-orange 100%);
+        border-radius: 12px;
+    }
+    .timer {
+        color: #8F8F8F;
+    }
+    .character {
+        font-family: OpenSans-Bold;
+        font-size: 2.5rem;
+    }
+    .alternative {
+        cursor: pointer;
+            transition: all .2s ease-out;
+            background: none;
+            box-shadow: 0 0 0 transparent;
+            border: 1px solid $white-smoke;
+            text-shadow: 0 0 0 transparent;
+            border-radius: 6px;
+        &:focus {
+            outline: none;
+            color: $forest-green !important;
+            border: 1px solid $forest-green;
+            border-radius: 6px;
+        }
+    }
+    .number-question {
+        border-radius: 50%;
+        width: 24px;
+        height: 24px;
+        background-color: $white-smoke;
+        font-family: OpenSans-Bold;
+        font-size: 12px;
+    }
+    .number-question-active {
+        background: linear-gradient(90deg, #FDA720 0%, $burning-orange 100%);
+        border: 2px solid $burning-orange;
+        color: white;
+    }
+</style>
+
 <script>
+// percent = 10*m/6
+import moment from 'moment'
 import Button from '../components/Button'
 import questions from '../assets/questions.json'
 export default {
@@ -32,14 +97,29 @@ export default {
     Button
   },
   data () {
+    const percent = '55%'
     const currentIndex = 0
     const questionCount = questions.length
-    console.log(currentIndex)
+    const finishTime = moment().add('60', 'm')
+    const timeRest = 0
     return {
+      timeRest,
+      finishTime,
+      percent,
       currentIndex,
       questionCount,
       questions
     }
+  },
+  methods: {
+    getRestTime () {
+      setInterval(() => {
+        this.timeRest = this.finishTime.diff(moment(), 'minutes')
+      }, 1000)
+    }
+  },
+  mounted () {
+    this.getRestTime()
   }
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,7 @@ module.exports = {
         prependData: `
           @import "@/styles/_variables.scss";
           @import "@/styles/_mixins.scss";
+          @import "@/styles/_functions.scss";
         `
       }
     }
