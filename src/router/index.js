fa39b79765d5289d709bed56c9a59d8c71b0e0f2
Paulo Italo <paulo.italo@esp.ce.gov.br>
2020-02-28 12:30:27 -0300

    #2 finish loading

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Welcome from '../views/Welcome.vue'
 import Question from '../views/Question.vue'
 import Success from '../views/Success.vue'
 import Confirmation from '../views/Confirmation.vue'
+import Loading from '../views/Loading.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -22,6 +23,11 @@ const routes = [
     path: '/question',
     name: 'Question',
     component: Question
+  },
+  {
+    path: '/loading',
+    name: 'Loading',
+    component: Loading
   },
   {
     path: '/confirmation',
