88ad1c75e6acffe3f88be6219c682c1ad3a54b18
Chicão Thiago <fthiagogv@gmail.com>
2020-09-11 15:34:46 -0300

    Atualizando texto da tela de bemvindo

---
+++ src/components/welcome/WelcomeBody.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="welcome-content">
+    <div class="welcome-sturcture">
+      <slot></slot>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WelcomeContent'
+}
+</script>
+
+<style scoped>
+  .welcome-content {
+    margin-left: 5rem;
+    margin-right: 5rem;
+    margin-bottom: 2rem;
+    padding: 2rem;
+    min-height: 300px;
+    height: 100%;
+    background-color: #fff;
+    border-radius: 20px;
+    box-shadow: 0px 10px 40px rgba(255, 225, 181, 0.5);
+  }
+  .welcome-sturcture {
+    font-size: 16pt;
+    letter-spacing: 1px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+</style>
