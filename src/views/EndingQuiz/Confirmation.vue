b7b0572ce7368b7a69af3337fe632d1fb7c8d2f5
JefersonNsoares <34108133+JefersonNSoares@users.noreply.github.com>
2021-08-04 17:09:06 -0300

    ✨  #36 - Adicionado ação do botao revisar

--- src/views/EndingQuiz/Confirmation.vue
+++ src/views/EndingQuiz/Confirmation.vue
@@ -2,7 +2,7 @@
   <BodyFinish>
     <template v-slot:actions>
       <div class='action-buttons-row'>
-        <NoButton @click="$router.push({ name: 'Question' })" />
+        <NoButton @click="onClick()" />
         <PurpleButton @click="$emit('finish')" label="ENVIAR RESPOSTAS" />
       </div>
     </template>
@@ -13,14 +13,24 @@
 import BodyFinish from './BodyFinish'
 import PurpleButton from '../../components/UX/PurpleButton'
 import NoButton from '../../components/UX/NoButton'
+import { mapActions } from 'vuex'
 
 export default {
   components: {
     BodyFinish,
     PurpleButton,
     NoButton
+  },
+  methods: {
+    ...mapActions('quiz', ['setCurrentQuestion']),
+    onClick () {
+      console.log('onClick')
+      this.setCurrentQuestion(0)
+      this.$router.push({ name: 'Question' })
+    }
   }
 }
+
 </script>
 
 <style lang='scss' scoped>
