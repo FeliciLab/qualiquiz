838d13c535213309f9bbe32ea41622c1327bf1f7
ericsonmoreira <moreira.ericson@gmail.com>
2021-09-10 11:59:37 -0300

    ⏪ revertendo mudancas indevidas na minha máquina

--- src/views/AllQuizzes.vue
+++ src/views/AllQuizzes.vue
@@ -1,26 +1,7 @@
 <template>
   <div>
-    <!-- TODO: passar isso para um component -->
-    <b-tabs fill justified no-nav-style content-class="teste">
-      <b-tab active>
-        <template #title>
-          <div class="title">
-            <b-icon-exclamation-triangle />
-            <span>Novas</span>
-          </div>
-        </template>
-        <p>{{ AllQuizzes }}</p>
-      </b-tab>
-      <b-tab title="Concluídas">
-        <template #title>
-          <div class="title">
-            <b-icon-check-circle-fill />
-            <span>Concluídas</span>
-          </div>
-        </template>
-        <p>{{ getUserQuizzes }}</p>
-      </b-tab>
-    </b-tabs>
+    <h1>{{ AllQuizzes }}</h1>
+    <h2>{{ getUserQuizzes }}</h2>
   </div>
 </template>
 
@@ -34,16 +15,4 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-.teste {
-  background-color: red;
-}
-
-.title {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 80px;
-}
-</style>
+<style lang="scss" scoped></style>
