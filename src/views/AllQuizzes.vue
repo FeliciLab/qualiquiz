527f448174c15f056a048bf6ca0f32c2c8b54dcf
ericsonmoreira <moreira.ericson@gmail.com>
2021-09-10 11:54:23 -0300

    🚧 fazendo testes para colocar as abas

--- src/views/AllQuizzes.vue
+++ src/views/AllQuizzes.vue
@@ -1,7 +1,26 @@
 <template>
   <div>
-    <h1>{{ AllQuizzes }}</h1>
-    <h2>{{ getUserQuizzes }}</h2>
+    <!-- TODO: passar isso para um component -->
+    <b-tabs fill justified no-nav-style content-class="teste">
+      <b-tab active>
+        <template #title>
+          <div class="title">
+            <b-icon-exclamation-triangle />
+            <span>Novas</span>
+          </div>
+        </template>
+        <p>{{ AllQuizzes }}</p>
+      </b-tab>
+      <b-tab title="Concluídas">
+        <template #title>
+          <div class="title">
+            <b-icon-check-circle-fill />
+            <span>Concluídas</span>
+          </div>
+        </template>
+        <p>{{ getUserQuizzes }}</p>
+      </b-tab>
+    </b-tabs>
   </div>
 </template>
 
@@ -15,4 +34,16 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.teste {
+  background-color: red;
+}
+
+.title {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 80px;
+}
+</style>
