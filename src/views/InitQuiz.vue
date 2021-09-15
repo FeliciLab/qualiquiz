6396f1686aa35e5e6765b9c51f036b27edbcfe5c
Laysa Cunha <laysacunha@gmail.com>
2021-09-15 11:19:28 -0300

    💄 #60 - Adiciona conteúdo e estiliza tela initQuiz (botão INICIAR)

--- src/views/InitQuiz.vue
+++ src/views/InitQuiz.vue
@@ -1,24 +1,38 @@
 <template>
   <div>
     <div v-show="showLoading">
-      <Loading />
+      <Loading></Loading>
     </div>
-    <h1>{{ getCurrentQuizId }}</h1>
-    <h1>{{this.getToken}}</h1>
-    <h1>teste</h1>
-    <div class="button">
-          <PurpleButton
-            @click="iniciarQuiz"
-            class="text-center"
-            label="INICIAR AVALIAÇÃO"
-          />
-        </div>
+    <div
+      class="background" id="loading"
+      v-show="!showLoading"
+    >
+      <div id="container-body">
+        <h5 class="text-title">
+          {{ nameQuiz }}
+        </h5>
+        <p class="quiz-description">
+          {{ description }}
+        </p>
+        <p id="body-text" class="roboto" v-html="bodyText"></p>
+      </div>
+      <div id="init-quiz-bottom">
+        <PurpleButton
+          @click="iniciarQuiz"
+          class="text-center"
+          id="formSubmit"
+          color="success"
+          label="INICIAR AVALIAÇÃO"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
 import PurpleButton from '../components/UX/PurpleButton.vue'
+import Swiper from 'swiper'
 import Loading from '../components/Loading.vue'
 export default {
   name: 'InitQuiz',
@@ -28,7 +42,10 @@ export default {
   },
   data () {
     return {
-      showLoading: false
+      showLoading: false,
+      bodyText: `
+      OBS: REVISAR ESSE TEXTO. Ao confimar esta ação você irá dar início a sua avaliação. É importante destacar que o tempo do processo avaliativo começará a ser contabilizado, mas, se por algum imprevisto você não conseguir completar a jornada de questões, você poderá retomar de onde parou em outro momento. No entanto, sua pontuação e tempo só serão contabilizados no histórico após a conclusão.
+      `
     }
   },
   computed: {
@@ -59,8 +76,44 @@ export default {
       devMode: this.getDevelopment,
       auth: this.getToken
     })
+    // eslint-disable-next-line no-new
+    new Swiper('.swiper-container', {
+      // If we need pagination
+      pagination: {
+        clickable: true,
+        el: '.swiper-pagination'
+      },
+      spaceBetween: -35
+    })
   }
 }
 </script>
-
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#loading{
+  display:flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100vh;
+}
+#container-body{
+  padding: 0 16px;
+}
+#init-quiz-bottom{
+  display: flex;
+  justify-content: flex-end;
+  padding: 0 16px 24px 0;
+}
+#body-text{
+  font-size: 14px;
+  color: $black64;
+}
+.text-title {
+  align-items: center;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  letter-spacing: 0.25px;
+  font-size: 34px;
+  padding: 16px 0 24px 0;;
+}
+</style>
