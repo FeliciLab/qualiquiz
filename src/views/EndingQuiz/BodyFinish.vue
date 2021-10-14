8b5dc2a9164cd6ee363ff6a3585ac2868823dca4
ericsonmoreira <moreira.ericson@gmail.com>
2021-10-14 10:46:56 -0300

    🐛 #76 corrigir telas de fim de quiz

--- src/views/EndingQuiz/BodyFinish.vue
+++ src/views/EndingQuiz/BodyFinish.vue
@@ -9,20 +9,18 @@
         <div class="circle-centro flex-column-items-center">
           <AmountAnsweredCard />
         </div>
-        <div class="flex-column-items-center circle-timeout">
-          <CircleTimeOut />
-        </div>
-        <p class="text-center text-answered roboto-bold">
+        <p class="mt-50 text-center text-answered">
           Você Respondeu {{ amountAnswers }} de {{ amountQuestions }} questões!
         </p>
 
-        <p class="text-helper text-center">
-          Você não conseguiu finalizar o teste a tempo, mas não se preocupe.
-          Volte ao início e aguarde uma próxima oportunidade!
+        <p class="mx-13 mt-18 text-helper text-center">
+          Você pode revisar as questões ou finalizar o quiz para ver o seu
+          resultado. Após enviar suas respostas, não será possível realizar
+          novas tentativas.
         </p>
       </div>
       <div class="button-row">
-        <PurpleButton @click="onClickInicio()" label="VOLTAR AO INÍCIO" />
+        <slot slot name="actions"></slot>
       </div>
     </div>
   </div>
@@ -40,6 +38,7 @@ export default {
     AmountAnsweredCard,
     BreadcrumbsFixed
   },
+
   computed: {
     ...mapGetters('quiz', {
       amountQuestions: 'getNumberOfQuestions',
@@ -50,23 +49,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.page-title {
-  font-size: 20px;
-  margin: 33px 16px 0 16px;
-}
-.circle-timeout {
-  margin-top: 56px;
-}
 .text-answered {
   color: #000000;
   font-size: 16px;
-  margin-top: 40px;
-  margin-bottom: 20px;
 }
 .text-helper {
   font-size: 14px;
   color: $black87;
-  margin: 25px 16px 0 16px;
 }
 .text-form {
   font-size: 16px;
@@ -81,10 +70,7 @@ export default {
   min-height: 100vh;
 }
 .button-row {
-  margin-bottom: 24px;
-  display: flex;
-  justify-content: flex-end;
-  padding-right: 16px;
+  margin-bottom: 1rem;
 }
 .title-fim-quiz {
   font-size: 20px;
--- src/views/EndingQuiz/TimeOut.vue
+++ src/views/EndingQuiz/TimeOut.vue
@@ -1,32 +1,29 @@
 <template>
-    <div
-      class='animated fadeIn'
-    >
-      <div class='tela-confirmacao'>
-        <div>
-          <div class="roboto-bold page-title">
-            Fim do quiz
-          </div>
-          <div class='flex-column-items-center circle-timeout'>
-            <CircleTimeOut />
-          </div>
-            <p class='text-center text-answered roboto-bold'>
-              Você Respondeu {{ amountAnswers }} de {{ amountQuestions }} questões!
-            </p>
-
-            <p class='text-helper text-center'>
-               Você não conseguiu finalizar o teste a tempo, mas não se preocupe. Volte ao início e aguarde uma próxima oportunidade!
-            </p>
-          </div>
-        <div class="button-row">
-          <PurpleButton @click="onClickInicio()" label="VOLTAR AO INÍCIO" />
+  <div class="animated fadeIn">
+    <div class="tela-confirmacao">
+      <div>
+        <div class="roboto-bold page-title">
+          Fim do quiz
         </div>
+        <div class="flex-column-items-center circle-timeout">
+          <CircleTimeOut />
+        </div>
+        <p class="text-center text-answered roboto-bold">
+          Você Respondeu {{ amountAnswers }} de {{ amountQuestions }} questões!
+        </p>
+        <p class="text-helper text-center">
+          Você não conseguiu finalizar o teste a tempo, mas não se preocupe.
+          Volte ao início e aguarde uma próxima oportunidade!
+        </p>
+      </div>
+      <div class="button-row">
+        <PurpleButton @click="onClickInicio()" label="VOLTAR AO INÍCIO" />
       </div>
     </div>
+  </div>
 </template>
 
 <script>
-
 import { mapGetters } from 'vuex'
 import CircleTimeOut from '../../components/Confirmation/CircleTimeOut.vue'
 import routerNames from '../../router/routerNames'
@@ -53,12 +50,12 @@ export default {
 }
 </script>
 
-<style lang='scss' scoped>
-.page-title{
+<style lang="scss" scoped>
+.page-title {
   font-size: 20px;
   margin: 33px 16px 0 16px;
 }
-.circle-timeout{
+.circle-timeout {
   margin-top: 56px;
 }
 .text-answered {
@@ -71,21 +68,20 @@ export default {
   font-size: 14px;
   color: $black87;
   margin: 25px 16px 0 16px;
-
 }
 .text-form {
   font-size: 16px;
   font-family: 'Roboto-Bold';
   text-align: center;
 }
-.tela-confirmacao{
+.tela-confirmacao {
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   height: 100%;
   min-height: 100vh;
 }
-.button-row{
+.button-row {
   margin-bottom: 24px;
   display: flex;
   justify-content: flex-end;
